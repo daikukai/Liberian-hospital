a39be6376559f86a71ef5981716cb29cd792f1ff
daikukai <30861385+daikukai@users.noreply.github.com>
2021-09-13 12:21:33 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,61 @@
+  <footer class="footer-2">
+    <div class="container">
+      <div class="row"> 
+        
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p>A concern of HealthCare delivery has been a paramount issue that has been discussed by the health aothority. The hospital management system is a tool to aid healthcare delivery in  Liberia. This system can be use by Hospitals, clinic and etc. </p>
+        </div>
+        
+       
+        
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <h6>Contact Us</h6>
+            <p>72nd SKD Boulevard, Paynesville City</p>
+            <ul>
+              <li class="mobile">
+                <p>0778445619</p>
+                <p>0555235751-HMS</p>
+              </li>
+              <li class="email">
+                <p>daikukaibindah@gmail.com</p>
+                <p>contact@libwellsolutions.com</p>              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© <?php echo date("Y"); ?> Hospital Management System - Developed By <span style="color: white">Libwell Corporation</span></p>
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
